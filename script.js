eea482eee4172a200885df6d1c7c424313621148
nitin3000ny <94776214+nitin3000ny@users.noreply.github.com>
2023-06-11 22:25:42 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const currentYear = new Date().getFullYear();
 const currentYearElement = document.getElementById('current-year');
-currentYearElement.textContent = currentYear; document.addEventListener("DOMContentLoaded", function () {
+currentYearElement.textContent = currentYear;
+document.addEventListener("DOMContentLoaded", function () {
     const popupOverlay = document.querySelector(".popup-overlay");
     const closeButton = document.querySelector(".close-btn");
 
@@ -9,3 +10,16 @@ currentYearElement.textContent = currentYear; document.addEventListener("DOMCont
         popupOverlay.style.display = "none";
     });
 });
+const hambutton = document.querySelector('#hamburger');
+hambutton.addEventListener('click', () => {
+    const menu = document.querySelector('#nav-links');
+    menu.classList.add('active');
+
+})
+const closebutton = document.querySelector('#close-menu-btn');
+closebutton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const menu = document.querySelector('#nav-links');
+    menu.classList.remove('active');
+
+})
